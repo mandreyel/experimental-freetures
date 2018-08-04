f88677e8a5a23cc0988c99ab142387beba349361
mandreyel <mandreyel@protonmail.com>
2018-08-04 15:11:54 +0200

    Add void specialization to promise and type traits

--- include/freetures/promise.hpp
+++ include/freetures/promise.hpp
@@ -34,6 +34,31 @@ public:
     void invoke_handler();
 };
 
+/** @brief Void specialization. */
+template<>
+class promise<void>
+{
+    std::shared_ptr<detail::shared_state<void>> state_;
+
+public:
+    /**
+     * Constructs the promise and its associated shared state.
+     */
+    promise() : state_(std::make_shared<detail::shared_state<void>>()) {}
+
+    /**
+     * @brief Returns a future associated with this promise.
+     */
+    future<void> get_future();
+
+    void set_value();
+    void set_error(std::error_code& error);
+    void set_timeout();
+
+    bool is_fulfilled() const;
+
+    void invoke_handler();
+};
 } // ft
 
 #endif
--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,5 @@
 #include "../include/freetures.hpp"
+#include <iostream>
 
 int main()
 {
